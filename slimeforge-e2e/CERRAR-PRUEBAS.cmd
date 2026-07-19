34def8b36b39c2fd94d481487b215d5ae9d2098b
Your Name <you@example.com>
2026-07-19 02:38:24 +0100

    tools: FAQ + schema + related links + versión ES (7 herramientas)

--- slimeforge-e2e/CERRAR-PRUEBAS.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0close-slimeforge-test.ps1"
-if errorlevel 1 pause
-
--- slimeforge-e2e/COMPROBAR-PRUEBAS.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0check-slimeforge-test.ps1"
-pause
-
